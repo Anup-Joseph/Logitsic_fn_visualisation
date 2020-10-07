a0b84a6f254752d9d27f43a3fd628ab1758544d1
Anup-Joseph <70029698+Anup-Joseph@users.noreply.github.com>
2020-10-07 15:58:16 +0530

    Update bifurcation_plot.m

--- bifurcation_plot.m
+++ bifurcation_plot.m
@@ -2,28 +2,23 @@ clear all; close all;
 xvalues=[];
 
 
-for r = 0:0.0001:4
-    xold = 0.5;
+for r = 0:0.0001:4  %growth rate 
+    xold = 0.5;     %initialise x value
     for i=1:2000
-        xnew = (xold-xold^2)*r;
-        xold = xnew;
+        xnew = (xold-xold^2)*r;     %logistic function x(n)=x(n-1)*r*(1-x(n-1))
+        xold = xnew;                %since the x value settles to a single value we iterate first 2000 to dtermine that value
     end
     xss=xnew;
-    for i=1:1000
+    for i=1:1000                        
         xnew = (xold-xold^2)*r;
-        xold=xnew;
+        xold=xnew;                          
         xvalues(1,length(xvalues)+1)=r;
-        xvalues(2,length(xvalues))=xnew;
-        if(abs(xss-xnew)<0.001)
+        xvalues(2,length(xvalues))=xnew;        
+        if(abs(xss-xnew)<0.001)             
             break;
         end
     end
-end
-for i=2:length(xvalues)-1
-    xvalues(3,i-1)=xvalues(2,i);
-end
-
-    
+end    
 
 plot(xvalues(1,:),xvalues(2,:),'.')
 xlabel('growth rate(r)');
